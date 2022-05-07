229a225be37d6010e92db83b79f49f51a3da4916
Tyagi <tyagia@vdi-sccubu-001.lancs.local>
2022-05-07 13:21:04 +0100

    specified the main funtion and created an empty window for the GUI

--- festival.java
+++ festival.java
@@ -5,5 +5,16 @@ import javax.swing.JButton;
 
 public class festival
 {
+    public static void main(String[] args) 
+    {   
+        JFrame frame  = new JFrame("festival");
 
+        frame.setLayout(null);
+
+        frame.setSize(900, 600);
+        frame.setTitle("Festival scheduling");
+        frame.setVisible(true);
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    } 
 }
