90b90656311c4facbd2cf6a3234ab0a7180425e5
Tyagi <tyagia@vdi-sccubu-006.lancs.local>
2022-05-08 19:24:16 +0100

    used a for loop and an array to create 4 input boxes for the festival information

--- festival.java
+++ festival.java
@@ -3,24 +3,51 @@ import java.awt.Color;
 import javax.swing.JLabel;
 import javax.swing.JButton;
 import javax.swing.JTextField;
+import java.awt.Font;
 
 public class festival
 {
-    public static void main(String[] args) 
-    {   
-        JFrame frame  = new JFrame("festival");
+    private JFrame frame ;
+    private JTextField fields[];
+    
+    
+    public festival()
+    {
+        frame  = new JFrame("festival");
 
         frame.setLayout(null);
 
-        frame.setSize(900, 600);
-        frame.setTitle("Festival scheduling");
+        frame.getContentPane().setBackground(Color.BLACK);
 
-        JTextField field1 = new JTextField("name");
-        field1.setBounds(50,100, 200,30);  
-        frame.getContentPane().add(field1);
+        JLabel label1 = new JLabel("Festival Scheduling");
+        label1.setForeground(Color.WHITE);
+        label1.setFont(new Font("Serif", Font.PLAIN, 30));
+        label1.setBounds(160, 0, 400, 200);
+        frame.getContentPane().add(label1);
 
+        frame.setSize(600, 600);
+        frame.setTitle("window");
         frame.setVisible(true);
 
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE); 
+    }
+
+    public static void main(String[] args) 
+    {   
+        festival fest = new festival();
+        fest.user_input();
     } 
+
+    public void user_input()
+    {
+        int pos = 140;
+        fields = new JTextField[4];
+        for (int i = 0; i <= 3; i++)
+        {
+            fields[i]= new JTextField();
+            fields[i].setBounds(300, pos, 160, 30);
+            pos = pos + 50;
+            frame.getContentPane().add(fields[i]);       
+        }
+    }
 }
